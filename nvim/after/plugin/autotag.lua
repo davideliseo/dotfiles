299aec0f825350645d9180623fb13a3beab64730
David Uribe <daviduribe998@gmail.com>
2023-07-24 02:09:32 -0400

    feat: Autotag y telescope file browser

---
+++ nvim/after/plugin/autotag.lua
@@ -0,0 +1,6 @@
+require('nvim-ts-autotag').setup {
+  enable = true,
+  enable_rename = false,
+  enable_close = true,
+  enable_close_on_slash = true,
+}
--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -5,7 +5,26 @@ vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({
-        search = vim.fn.input('Grep String > ')
-    })
+  builtin.grep_string({
+    search = vim.fn.input('Grep String > ')
+  })
 end, {})
+
+vim.keymap.set('n', '<C-i>', builtin.lsp_document_symbols, {})
+
+require('telescope').setup{
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ["<C-h>"] = "which_key"
+      }
+    }
+  },
+}
+
+require("telescope").load_extension "file_browser"
